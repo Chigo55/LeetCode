3e5ebeb78b947a07cb52e1ac4a67d76f36e45fb6
Chigo55 <97353021+Chigo55@users.noreply.github.com>
2024-01-29 17:29:09 +0900

    Time: 277 ms (95.83%) | Memory: 36.9 MB (62.85%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -3,9 +3,11 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import collections
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        q: List = []
+        q: Deque = collections.deque()
 
         if not head:
             return True
@@ -17,7 +19,7 @@ class Solution:
             node = node.next
 
         while len(q) > 1:
-            if q.pop(0) != q.pop():
+            if q.popleft() != q.pop():
                 return False
         
         return True
