ecce31def905e08023f97f34a154801ec57e3f45
Chigo55 <97353021+Chigo55@users.noreply.github.com>
2024-01-29 16:35:15 +0900

    Time: 163 ms (83.94%) | Memory: 23.8 MB (85.97%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -8,7 +8,7 @@ class Solution:
             p = p * nums[i]
         p = 1
 
-        for i in range(len(nums)-1, 0-1, -1):
+        for i in range(len(nums)-1, -1, -1):
             out[i] = out[i] * p
             p = p * nums[i]
         
