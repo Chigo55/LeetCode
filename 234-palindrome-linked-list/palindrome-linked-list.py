702498432846dcca103e77555df3a780563f7b43
Chigo55 <97353021+Chigo55@users.noreply.github.com>
2024-01-29 17:36:22 +0900

    Time: 251 ms (99.82%) | Memory: 27.4 MB (99.30%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -3,23 +3,17 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import collections
-
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        q: Deque = collections.deque()
-
-        if not head:
-            return True
+        rev = None
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
         
-        node = head
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
 
-        while node is not None:
-            q.append(node.val)
-            node = node.next
-
-        while len(q) > 1:
-            if q.popleft() != q.pop():
-                return False
-        
-        return True
+        return not rev
